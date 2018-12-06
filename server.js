078c106c6e1fcba6e45cbd6297671b60669dcc42
Andy Prudhomme <aprudhomme9@gmail.com>
2018-12-05 20:26:27 -0600

    fuck you

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const originRoute = process.env.CLIENT_URL || 'http://localhost:3000';
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-	origin: originRoute,
+	origin: process.env.CLIENT_URL,
 	credentials: true,
 	optionsSuccessStatus: 200
 }
