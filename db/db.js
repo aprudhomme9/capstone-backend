08f504087fc7f1f0c8585e5caacf226b3356deb3
Andy Prudhomme <aprudhomme9@gmail.com>
2018-12-05 18:54:08 -0600

    more heroku

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = proces.env.MONGODB_URI || 'mongodb://localhost/watchWithFriends';
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/watchWithFriends';
 
 
 mongoose.connect(connectionString, { useNewUrlParser: true});
