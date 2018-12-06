23cfe5ba7b4a3c183b9a041fc8b6a34bd59e293c
Andy Prudhomme <aprudhomme9@gmail.com>
2018-12-05 20:14:37 -0600

    more

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(bodyParser.json())
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+    res.setHeader('Access-Control-Allow-Origin', 'https://watch-with-friends-express.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
