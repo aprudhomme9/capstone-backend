8e38a59e833fa5c098188fd8e3aff0fc913cd55c
Andy Prudhomme <aprudhomme9@gmail.com>
2019-01-30 17:09:08 -0600

    x

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const originRoute ='https://watch-with-friends-react.herokuapp.com'
  ;
 // const originRoute = 'http://locahost:3000';
 
-const PORT = 5000 || 'https://watch-with-friends-express.herokuapp.com/';
+const PORT = 5000 || process.env.PORT;
 console.log(process.env, '<----process.env');
 
 
@@ -23,14 +23,33 @@ console.log(originRoute, '<---OG ROUTE');
 require('./db/db')
 
 var whitelist = ['http://locahost:3000', originRoute]
+
 const corsOptions = {
-  origin: false,
-  methods: ["GET","PUT","PATCH","POST","DELETE"],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Origin"],
-  credentials: true
+    origin: whitelist,
+    credentials: true,
+    optionsSuccessStatus: 200
 }
+app.use(cors(corsOptions));
+
+// cors headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', true);
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 
 app.use(session({
@@ -67,7 +86,7 @@ app.use('/api/recs', recController);
 app.use('/api/showrecs', showRecController);
 app.use('/auth', authController);
 
-app.use(cors(corsOptions));
+
 
 app.listen(PORT, () => {
 	console.log('listening on port ' + PORT)
